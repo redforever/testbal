17f830b06cdd1bc212c2e56efc3aee40d05ca42f
red red <redforever258654@gmail.com>
2019-01-19 08:01:58 +0100

    first

---
+++ serv.js
@@ -0,0 +1,21 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+var port = process.env.PORT || 8080;
+
+app.get('/Acc', function (req, res) {/* si je saisie l adresse localhost:8081/Acc je charge la page indexAcc.html*/
+  res.sendfile(__dirname + '/indexAcc.html');
+});
+
+app.get('/Can', function (req, res) {/* si je saisie l adresse localhost:8081/Acc je charge la page indexCan.html*/
+  res.sendfile(__dirname + '/indexCan.html');
+});
+
+io.sockets.on('connection', function (socket) {//je demarre la connexion
+    socket.on('message', function (message) {//je recois le message par la socket
+        io.emit('position',message);//je difuse le message à l'ensemble des pages
+    });
+});
+
+server.listen(port);//le serveur ecoute sur le port 8081
